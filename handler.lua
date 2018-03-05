e97ecd464ad77824a8cf36453bc7fa30ccb79c4a
Fadel <pfadel@stone.com.br>
2018-03-05 15:32:20 -0300

    :rocket: FAZ FUNCIONAR

--- handler.lua
+++ handler.lua
@@ -1,5 +1,6 @@
 local BasePlugin = require "kong.plugins.base_plugin"
 local http = require 'resty.http'
+local cjson = require "cjson"
 
 local MiddlewareHandler = BasePlugin:extend()
 
@@ -14,15 +15,25 @@ function MiddlewareHandler:access(config)
 
   local httpc = http:new()
   local headers = ngx.req.get_headers()
+  ngx.req.clear_header('Host')
+  headers['Content-Type'] = "application/json"
+  
+  -- Executa o request http
   local res, err = httpc:request_uri(config.url, {
     method = "POST",
+    ssl_verify = false,
+    headers = headers,
     body = string.format("{\"ApplicationKey\": %q, \"Token\": %q}", config.appKey, headers["token"])
   })
+  
+  -- Erro Durante o request
   if err ~= nil then
     ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
     ngx.say(err)
     return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
   end
+  
+  -- Status code inesperado
   if res.status > 299 then
     ngx.status = res.status
     for key, value in pairs(res.headers) do
@@ -30,6 +41,17 @@ function MiddlewareHandler:access(config)
     end
     ngx.say(res.body)
     return ngx.exit(res.status)
+  end
+
+  -- Parseia o resultado e avalia o sucesso
+  local data = cjson.decode(res.body)
+  if not data["Success"] then
+    ngx.status = ngx.HTTP_UNAUTHORIZED
+    for key, value in pairs(res.headers) do
+      ngx.header[key] = value
+    end
+    ngx.say(res.body)    
+    return ngx.exit(ngx.HTTP_UNAUTHORIZED)
   end
 end
 
